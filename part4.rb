b3aea2cdf5bdc45e2115e22bf88ad48ad75db591
senquevila <senquevila@gmail.com>
2013-02-01 11:18:40 -0600

    Ejercicio 2.4 Terminado

--- part4.rb
+++ part4.rb
@@ -1,23 +1,75 @@
 class Dessert
+  attr_accessor :name, :calories
+  
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
+#  def name=(name)
+#    @name = name
+#  end
+#  
+#  def calories=(calories)
+#    @calories = calories
+#  end
+#  
+#  def name
+#    @name
+#  end
+#  
+#  def calories
+#    @calories
+#  end
+#  
   def healthy?
-    # YOUR CODE HERE
+    if (@calories < 200)
+      return true
+    else
+      return false
+    end
   end
   
   def delicious?
-    # YOUR CODE HERE
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :name, :calories, :flavor
+  
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    super(name, calories)
+    @flavor = flavor
   end
   
+#  def flavor=(flavor)
+#    @flavor = flavor
+#  end
+#  
+#  def flavor
+#    @flavor
+#  end
+  
   def delicious?
-    # YOUR CODE HERE
+    if (@flavor == "black licorice")
+      return false
+    else
+      return true
+    end
   end
 end
+
+nombre = "test"
+calorias = 190
+sabor = "coc"
+a = JellyBean.new(nombre, calorias, sabor)
+
+print a.flavor()
+
+
+##if (delicious?(a.flavor))
+##  print "rico"
+##else
+##  print "esta malo"
+##end
